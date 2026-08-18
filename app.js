21eefe63ebdc49feedaec24585979256575fd043
eklem <espen.klem@gmail.com>
2026-08-18 08:01:58 +0200

    sending command + JSON object to worker-server by fetch()

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ if ("serviceWorker" in navigator) {
   console.error("Service workers are not supported.")
 }
 
+
 /* ### Calculate button action */
 const calculateButton = document.getElementById('calculate')
 
@@ -42,4 +43,5 @@ calculateButton.addEventListener('click', (event) => {
   const arithmeticSymbol = document.getElementById('arithmeticSymbol').value
   const secondNumber = document.getElementById('secondNumber').value
   console.log('calculating: ' + firstNumber + ' ' + arithmeticSymbol + ' ' + secondNumber)
+  fetch(window.location.origin + window.location.pathname + 'API?' + arithmeticSymbol + '={"firstNumber": "' + firstNumber + '", "secondNumber": "' + secondNumber + '"}')
 })
